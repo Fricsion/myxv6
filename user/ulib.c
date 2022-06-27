6652bebc4ed8841ac9ee706e45c4398b9f62f067
root <root@localhost.localdomain>
2022-06-27 06:14:50 +0000

    ライセンス関係をはっきりさせました

--- user/ulib.c
+++ user/ulib.c
@@ -136,6 +136,33 @@ memcpy(void *dst, const void *src, uint n)
 }
 
 // MD5-related functions
+// MD5-related functions
+// Reference: RSA Data Security, Inc. MD5 Message Digest Algorithm
+// http://www.efgh.com/software/md5.htm (2002) 
+
+/*
+Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
+rights reserved.
+
+License to copy and use this software is granted provided that it
+is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+Algorithm" in all material mentioning or referencing this software
+or this function.
+
+License is also granted to make and use derivative works provided
+that such works are identified as "derived from the RSA Data
+Security, Inc. MD5 Message-Digest Algorithm" in all material
+mentioning or referencing the derived work.
+
+RSA Data Security, Inc. makes no representations concerning either
+the merchantability of this software or the suitability of this
+software for any particular purpose. It is provided "as is"
+without express or implied warranty of any kind.
+
+These notices must be retained in any copies of any part of this
+documentation and/or software.
+*/
+
 #define F(x, y, z) (((x) & (y)) | ((~x) & (z)))
 #define G(x, y, z) (((x) & (z)) | ((y) & (~z)))
 #define H(x, y, z) ((x) ^ (y) ^ (z))
